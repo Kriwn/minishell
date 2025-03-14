432b26234a973556579b494d5822f4e97180d427
Tisarji <jirasitkarunwong@gmail.com>
2025-03-14 16:16:43 +0700

    update

--- srcs/parser/parsing_heredoc.c
+++ srcs/parser/parsing_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: jikarunw <jikarunw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 12:03:40 by jikarunw          #+#    #+#             */
-/*   Updated: 2025/03/14 15:16:46 by jikarunw         ###   ########.fr       */
+/*   Updated: 2025/03/14 16:14:35 by jikarunw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,12 @@ t_ast	*create_heredoc_word_node(t_token **token)
 	heredoc_word_node->args = malloc(sizeof(char *) * 2);
 	if (!heredoc_word_node->args)
 	{
-		free(heredoc_word_node);
+		free_cmd_args(heredoc_word_node);
+		free_ast(heredoc_word_node);
 		return (NULL);
 	}
-	if (*token && (*token)->cmd)
-		heredoc_word_node->args[0] = ft_strdup((*token)->cmd);
-	else
-		heredoc_word_node->args[0] = ft_strdup("");
+	heredoc_word_node->args[0] = ft_strdup((*token)->cmd);
 	heredoc_word_node->args[1] = NULL;
-	*token = (*token)->next;
 	return (heredoc_word_node);
 }
 
@@ -58,25 +55,18 @@ t_ast	*msh_get_heredoc_word(t_token **token)
 
 	if (!token || !*token || (*token)->type != HEREDOC)
 		return (NULL);
-
 	heredoc_node = create_heredoc_node(token);
 	if (!heredoc_node)
 		return (NULL);
-	*token = (*token)->next;
-	if (!*token || (*token)->type != CMD)
-	{
-		free_ast(heredoc_node);
-		return (NULL);
-	}
 	heredoc_word_node = create_heredoc_word_node(token);
 	if (!heredoc_word_node)
 	{
+		free_cmd_args(heredoc_word_node);
 		free_ast(heredoc_node);
 		return (NULL);
 	}
 	heredoc_node->right = heredoc_word_node;
-	free((*token)->cmd);
-	free(*token);
+	next_token = (*token)->next;
 	*token = next_token;
 	if (*token && (*token)->type == HEREDOC)
 		heredoc_node->left = msh_get_heredoc_word(token);
